ded403fe2aa2d84efcb4eb45ecf38c4d89b34e42
ivanbielozertsev <ivanbelozercev13@gmail.com>
2021-10-21 17:35:10 +0300

    Home Work 10

---
+++ js/scripts.js
@@ -0,0 +1,69 @@
+// const a = +prompt('Введите переменную');
+// const nullCheck = a === 0 ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const nullCheck = a > 0 ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const nullCheck = a < 0 ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const nullCheck = a >= 0 ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const nullCheck = a <= 0 ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const nullCheck = a !== 0 ? alert('Верно') : alert('Неверно');
+
+// const a = prompt('Введите переменную');
+// const testCheck = a === 'test' ? alert('Верно') : alert('Неверно');
+
+// const a = prompt('Введите переменную');
+// const testCheck = a === '1' ? alert('Верно') : alert('Неверно');
+
+// const a = +prompt('Введите переменную');
+// const intervalCheck = 0 < a && a < 5 ? alert('Верно') : alert('Неверно');
+
+// let a = +prompt('Введите переменную');
+// if (a === 0 || a === 2) {
+//     a += 7;
+//    }; else {
+//     a /= 10;
+//    };
+//    alert(a)
+
+// let a = +prompt('Введите переменную "a"');
+// let b = +prompt('Введите переменную "b"');
+// let sum = 0;
+// if (a <= 1 && b>=3) {
+//     sum = a + b;
+// } else {
+//     sum = a - b;
+// };
+// alert (sum)
+
+// let a = +prompt('Введите переменную "a"');
+// let b = +prompt('Введите переменную "b"');
+// if (a > 2 && a < 3 || b >=6 && b < 14) {
+//     alert('Верно');
+// } else {
+//     alert('Неверно');
+// }
+
+// let a = +prompt('Введите переменную "a"');
+// switch (a) {
+//     case a = 1:
+//         season = 'Зима';
+//     break;
+//     case a = 2:
+//         season = 'Весна';
+//     break;
+//     case a = 3:
+//         season = 'Лето';
+//     break;
+//     case a = 4:
+//         season = 'Осень';
+//     break;
+// }
+// alert(season)
